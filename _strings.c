67300c618f5d9cfe2c76caeb2c6d5aecff7da414
frank-gich <gichuhi5781@gmail.com>
2022-08-11 00:24:08 +0300

    strings

--- _strings.c
+++ _strings.c
@@ -1,95 +1,112 @@
 #include "holberton.h"
 
 /**
- * _setenv - updates or adds an environment variable
- * @var_name:variable name
- * @var_value:variable value
- * Return:0 - success,otherwise -1
+ * _strlen - Returns length of a string
+ * @s: Pointer to string
+ * Return: Length of s
  */
-int _setenv(char *var_name, char *var_value)
+int _strlen(char *s)
 {
-	int i = 0;
-	size_t name_len;
-	char *var_new;
+	int index;
 
-	name_len = _strlen(var_name);
-	i = 0;
-	/*updating an existing variable*/
-	while (environ[i])
+	index = 0;
+	while (s[index] != '\0')
 	{
-		if (strncmp(environ[i], var_name, name_len) == 0)
-		{
-			var_new = var_build(var_name, var_value);
-			/*Not sure but wanted to clear its mem b4 writing*/
-			environ[i] = NULL;
-			environ[i] = _strdup(var_new);
-			free(environ[i]);
-			environ[i] = _strdup(var_new);
-			free(var_new);
-			return (0);
-		}
-		i++;
+		index++;
 	}
-	/*adding a variable that never existed before*/
-	var_new = var_build(var_name, var_value);
-	free(environ[i]);
-	environ[i] = _strdup(var_new);
-	i++;
-	environ[i] = NULL;
-	var_new = NULL;
-
-	return (0);
+	return (index);
 }
 /**
- * var_build - builds an environment variable from its name and valu
- * @var_name:variable name
- * @var_value:variable value
- * Return:string containing full environment variable
+ * _strcat - Concatenates two strings
+ * @dest: Pointer to string to be conatenated upon
+ * @src: Pointer to string to append to dest
+ * Return: Pointer to dest
  */
-char *var_build(char *var_name, char *var_value)
+char *_strcat(char *dest, char *src)
 {
-	char *new_var;
-	size_t var_len;
+	char *destAddress;
+	int destLen;
 
-	var_len = _strlen(var_name) + _strlen(var_value) + 2;
-	new_var = malloc(sizeof(char) * var_len);
-	if (new_var == NULL)
+	destAddress = dest;
+	destLen = _strlen(dest);
+	destAddress = destAddress + destLen;
+	while (*src != '\0')
 	{
-		perror("Error: Insufficient memory\n");
-		return (NULL);
+		*destAddress = *src;
+		src++;
+		destAddress++;
 	}
-	memset(new_var, 0, var_len);
-	/*Data in the form: var_name=var_value*/
-	new_var = _strcat(new_var, var_name);
-	new_var = _strcat(new_var, "=");
-	new_var = _strcat(new_var, var_value);
+	*destAddress = '\0';
 
-	return (new_var);
+	return (dest);
 }
+
 /**
- * _unsetenv - removes an environment variable
- * @var_name:variable name
- * Return:0 if successful -1,otherwise
+ * _check_white_space - checks for white space
+ * @s: Pointer to string to check
+ * Return: integer
  */
-int _unsetenv(char *var_name)
+unsigned int _check_white_space(char *s)
+{
+	int i, count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i]  == ' ' || s[i] == '\t' || s[i] == '\n')
+			count++;
+	}
+	return (count);
+}
+
+/**
+ * _strtotokens - splits a string into words
+ * @str: Pointer to string
+ * Return: Pointer to array of words
+ */
+char **_strtotokens(char *str)
 {
 	int i = 0;
-	char **env_temp;
-	size_t name_len;
+	const char delimeter[] = " \t\n";
+	int space = _check_white_space(str);
+	char **tokens = malloc(sizeof(char *) * (space + 1));
+	char *token;
 
-	name_len = _strlen(var_name);
-	while (environ[i])
+	if (!tokens)
 	{
-		if (strncmp(environ[i], var_name, name_len) == 0)
-		{
-			env_temp = environ;
-			free(env_temp[0]);
-			do {
-				env_temp[0] = env_temp[1];
-				env_temp++;
-			} while (*env_temp);
-		}
+		free(tokens);
+		fprintf(stderr, "sh: allocation error\n");
+		exit(1);
+	}
+
+	token = strtok(str, delimeter);
+
+	while (token != NULL)
+	{
+		tokens[i] = token;
+		token = strtok(NULL, delimeter);
 		i++;
 	}
-	return (0);
+	tokens[i] =  NULL;
+
+	return (tokens);
+}
+
+
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
+ */
+
+void _puts(char *str)
+{
+	unsigned long i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
 }
